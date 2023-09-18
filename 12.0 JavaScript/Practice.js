36223e281f569fd609a505b5be0eddb10bebd3c7
Tihan Enslin <tihanenslin@gmail.com>
2023-09-18 13:56:46 +0200

    the doing for javaScript coding

--- 12.0 JavaScript/Practice.js
+++ 12.0 JavaScript/Practice.js
@@ -43,3 +43,41 @@ if (loveScore === 100) {
 } else if (loveScore <=30) {
     alert("Love score: " + loveScore + "%" + " you go together like oil and water");
 }
+
+function bmiCalculator (weight, height) {
+    var BMI = (Math.round((weight / (Math.pow(height, 2))) * 10)) / 10;
+    if (BMI < 18.5) {
+        interpretation = "Your BMI is " + BMI + ", so you are underweight."
+    } else if (BMI >= 18.5 && BMI <= 24.9) {
+        interpretation = "Your BMI is " + BMI + ", so you have a normal weight."
+    } else if (BMI > 24.9) {
+        interpretation = "Your BMI is " + BMI + ", so you are overweight."
+    }
+    return interpretation;
+}
+
+console.log(bmiCalculator(100, 1.8));
+
+function isLeap(year) {
+    var answer = "Not leap year."
+    if ((year % 4) === 0){
+        answer = "Leap year."
+        if ((year % 100) === 0) {
+            answer = "Not leap year."
+            if ((year % 400) === 0) {
+                answer = "Leap year."
+            }
+        }
+    }
+    return answer;
+}
+
+console.log(isLeap(40));
+
+
+function whosPaying(names) {
+    var rnd = Math.round((Math.random()*(names.length - 1)));
+    var out = names[rnd] + " is going to buy lunch today!" 
+    return out;
+}
+
