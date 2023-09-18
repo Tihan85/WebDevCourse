d826b6bfa013f8d6c5db1ad4a0564cb8b4a967ce
Tihan85 <tihanenslin@gmail.com>
2023-09-18 14:01:42 +0200

    the doing for javaScript side of the course

--- 12.0 JavaScript/Practice.js
+++ 12.0 JavaScript/Practice.js
@@ -81,3 +81,21 @@ function whosPaying(names) {
     return out;
 }
 
+
+
+var output = [];
+var count = 1;
+
+function fizzBuzz() {
+        if ((count%3) == 0 && (count % 5) != 0) {
+            output.push("Fizz");
+        } else if ((count%5) == 0 && (count%3) != 0) {
+            output.push("Buzz");
+        } else if ((count%3) == 0 && (count%5) == 0 && count != 0) {
+            output.push("FizzBuzz")
+        } else {
+            output.push(count)
+        }
+    count++;
+    console.log(output);
+    }
