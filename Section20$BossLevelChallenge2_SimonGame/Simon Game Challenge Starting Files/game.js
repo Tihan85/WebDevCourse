8512ec7fbe668e884888c2b49fd81a3ef0632628
Tihan85 <tihanenslin@gmail.com>
2023-09-28 18:18:40 +0200

    step 4

--- Section20$BossLevelChallenge2_SimonGame/Simon Game Challenge Starting Files/game.js
+++ Section20$BossLevelChallenge2_SimonGame/Simon Game Challenge Starting Files/game.js
@@ -1,43 +1,30 @@
-
-/* Random number generating (Step2) */
+/* All the variables to store before hand */
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickPattern = [];
+
+/* Random number generating (Step2) */
+
 
 function nextSequence() {
    var randomNumber = Math.floor(Math.random() * 3);
    var randomChosenColor = buttonColors[randomNumber];
    gamePattern.push(randomChosenColor);
-   $(randomChosenColor).animate({opacity: 0.1}, 100).animate({opacity: 1});
-   var aud = new Audio("./sounds/" + randomChosenColor + ".mp3"); 
-   aud.play();
+   animations(randomChosenColor);
  }
 
-const btnClicked = $(".btn");
-btnClicked.click(function () {
-   var classes = btnClicked.attr("class").split(" ");
-   var userChosenColor = classes[1];
-   alert("button clicked: " + userChosenColor);
+/* Animations for when buttons are clicked */
+function animations(color){
+   var aud = new Audio("./sounds/" + color + ".mp3"); 
+   aud.play();
+   $("#" + color).animate({opacity: 0.1}, 100).animate({opacity: 1});
+};
+ 
+/* Audio for when buttons are pressed: */
+$(".btn").click(function () {
+   var userChosenColor = $(this).attr("class").split(" ")[1];
+   userClickPattern.push(userChosenColor);
+   console.log(userClickPattern);
+   animations(userChosenColor);
 });
 
-/* Audio for when buttons are pressed: */
-/* $("#blue").click(function (){
-    var blueA = new Audio("./sounds/blue.mp3"); 
-    blueA.play();
-    $("#blue").animate({opacity: 0.1}, 100).animate({opacity: 1});
- });
-$("#green").click(function (){
-    var greenA = new Audio("./sounds/green.mp3"); 
-    greenA.play();
-    $("#green").animate({opacity: 0.1}, 100).animate({opacity: 1});
- });
-$("#red").click(function (){
-   var redA = new Audio("./sounds/red.mp3"); 
-   redA.play();
-   $("#red").animate({opacity: 0.1}, 100).animate({opacity: 1});
-});
-$("#yellow").click(function (){
-    var yellowA = new Audio("./sounds/yellow.mp3"); 
-    yellowA.play();
-    $("#yellow").animate({opacity: 0.1}, 100).animate({opacity: 1});
- });
-*/
