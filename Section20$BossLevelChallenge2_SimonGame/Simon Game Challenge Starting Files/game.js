3f06705dc615e45bdd374800aef02e67dd0fb5f4
Tihan85 <tihanenslin@gmail.com>
2023-09-29 11:15:42 +0200

    Finished the Simon game

--- Section20$BossLevelChallenge2_SimonGame/Simon Game Challenge Starting Files/game.js
+++ Section20$BossLevelChallenge2_SimonGame/Simon Game Challenge Starting Files/game.js
@@ -3,11 +3,10 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickPattern = [];
 let keyPressed = false;
-let level = 0;
+var level = 0;
+count = 0;
 
 /* Random number generating (Step2) */
-
-
 function nextSequence() {
    var randomNumber = Math.floor(Math.random() * 3);
    var randomChosenColor = buttonColors[randomNumber];
@@ -28,20 +27,55 @@ function animations(color){
 $(".btn").click(function () {
    var userChosenColor = $(this).attr("class").split(" ")[1];
    userClickPattern.push(userChosenColor);
-   console.log(userClickPattern);
    animations(userChosenColor);
+   checkAnswer(level);
 });
 
 /* Step 6 Starting the game: */
 $(document).keypress(function(event) {
    if (!keyPressed) {
-      nextSequence();
       keyPressed = true;
+      nextSequence();
       // $("#level-title").html("Level " + level);
    }
 });
 
-/* Checking the answers */ 
-function checkAnswer() {
-   currentLevel = level;
+/* Checking the answers Step 8 */ 
+function checkAnswer(currentLevel) {
+   var cont = false;
+   if (gamePattern[count] == userClickPattern[count]){
+      count++;
+      if (count == currentLevel) {
+         cont = true;
+      }
+   } else {
+      console.log("wrong");
+      var wrong = new Audio("./sounds/wrong.mp3");
+      wrong.play();
+      $("#level-title").html("Game Over, Press Any Key to Restart");
+      $("body").addClass("game-over");
+      setTimeout(() => {
+         $("body").removeClass("game-over");
+      }, 200);
+      document.body.style.backgroundImage = "url('./sounds/IMG_8558.jpg')"
+      setTimeout(() => {
+         document.body.style.backgroundImage = "none";
+      }, 2000);
+      restart();
+   }
+   if (cont){
+      setTimeout(() => {
+         nextSequence();
+      }, 500);
+      userClickPattern = [];
+      count = 0;
+   }
+}
+
+function restart() {
+   level = 0;
+   count = 0;
+   gamePattern = [];
+   userClickPattern = [];
+   keyPressed = false;
 }
