33309598e5f4c122a324e17ff1dc10e5a5b0b3b1
Tihan85 <tihanenslin@gmail.com>
2024-01-09 13:11:32 +0200

    Section 2.3

---
+++ Section23$Node.js/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// var generateName = require('sillyname');
+
+// import xxx from "sillyname";
+import generateName from "sillyname";
+
+var sillyName = generateName();
+
+console.log(`My name is ${sillyName}.`);
